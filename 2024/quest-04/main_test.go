d5e9c3310cbc23b5c85383d100ea2521f5d86493
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2025-06-07 14:52:31 -0600

    Solve quest 4 part 3 in Go.

--- 2024/quest-04/main_test.go
+++ 2024/quest-04/main_test.go
@@ -3,11 +3,48 @@ package quest_04
 import (
 	"bufio"
 	"iter"
+	"math"
 	"os"
 	"sort"
 	"strconv"
 	"testing"
 )
+
+func assertEqual(t *testing.T, a, b any) {
+	if a != b {
+		t.Helper()
+		t.Errorf("%v != %v", a, b)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	assertEqual(t, 10, levelToLowest("part1-sample.txt"))
+	assertEqual(t, 76, levelToLowest("part1-full.txt"))
+}
+func TestPart2(t *testing.T) {
+	assertEqual(t, 923412, levelToLowest("part2-full.txt"))
+}
+func TestPart3(t *testing.T) {
+	assertEqual(t, 8, levelBruteForce("part3-sample.txt"))
+	assertEqual(t, 126398193, levelBruteForce("part3-full.txt"))
+}
+
+func levelBruteForce(filename string) (result int) {
+	result = 0xFFFFFFFF
+	heights := parseHeights(filename)
+	for _, height := range heights {
+		candidate := countHitsToAlignHeights(heights, height)
+		if candidate < result {
+			result = candidate
+		}
+	}
+	return result
+}
+func levelToLowest(filename string) int {
+	heights := parseHeights(filename)
+	sort.Ints(heights)
+	return countHitsToAlignHeights(heights, heights[0])
+}
 
 func lines(path string) iter.Seq[string] {
 	return func(yield func(string) bool) {
@@ -24,7 +61,6 @@ func lines(path string) iter.Seq[string] {
 		}
 	}
 }
-
 func parseInt(s string) int {
 	i, err := strconv.Atoi(s)
 	if err != nil {
@@ -32,26 +68,16 @@ func parseInt(s string) int {
 	}
 	return i
 }
-
-func TestPart1(t *testing.T) {
-	t.Log(level("part1-sample.txt"))
-	t.Log(level("part1-full.txt"))
-}
-func TestPart2(t *testing.T) {
-	t.Log(level("part2-full.txt"))
-}
-
-func level(filename string) int {
+func parseHeights(filename string) []int {
 	var heights []int
 	for line := range lines(filename) {
 		heights = append(heights, parseInt(line))
 	}
-	sort.Ints(heights)
-	target := heights[0]
-	heights = heights[1:]
-	hits := 0
+	return heights
+}
+func countHitsToAlignHeights(heights []int, target int) (result int) {
 	for _, height := range heights {
-		hits += height - target
+		result += int(math.Abs(float64(height) - float64(target)))
 	}
-	return hits
+	return result
 }
