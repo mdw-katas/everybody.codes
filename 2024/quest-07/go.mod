baa04ef69d2dd84b030f736c67d9e9bfcba8d027
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2025-08-20 00:14:15 -0600

    First phase of parsing quest 7 part 2.

--- 2024/quest-07/go.mod
+++ 2024/quest-07/go.mod
@@ -3,3 +3,5 @@ module github.com/mdw-katas/everybody.codes/2024/quest-07
 go 1.25
 
 require github.com/mdw-go/testing/v2 v2.0.0
+
+require github.com/mdw-go/generic-list v0.1.0 // indirect
--- 2024/quest-07/main_test.go
+++ 2024/quest-07/main_test.go
@@ -3,11 +3,14 @@ package quest_07
 import (
 	"bufio"
 	"bytes"
+	"io"
+	"log"
 	"os"
 	"sort"
 	"strings"
 	"testing"
 
+	"github.com/mdw-go/generic-list/list"
 	"github.com/mdw-go/testing/v2/assert"
 	"github.com/mdw-go/testing/v2/should"
 )
@@ -73,4 +76,52 @@ func (this *Track) Run(stepCount, power int) {
 		power += this.Steps[x%len(this.Steps)]
 		this.Result += power
 	}
+}
+
+var sampleTrack = strings.NewReader(strings.TrimSpace(`
+S+===
+-   +
+=+=-+
+`))
+
+func TestPart2(t *testing.T) {
+	log.SetOutput(t.Output())
+	assert.So(t, ParseLoopTrack(sampleTrack), should.Equal, "S+===++-=+=-")
+}
+
+func ParseLoopTrack(track io.Reader) string {
+	segments := list.New[rune]()
+	scanner := bufio.NewScanner(track)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if segments.Len() == 0 {
+			for _, c := range line {
+				segments.PushBack(c)
+			}
+		} else if strings.Count(line, " ") > 0 {
+			segments.PushFront(rune(line[0]))
+			segments.PushBack(rune(line[len(line)-1]))
+			log.Println(render(segments))
+		} else {
+			for _, c := range line {
+				segments.PushFront(c)
+			}
+		}
+	}
+	result := render(segments)
+	return result
+}
+
+func render(segments *list.List[rune]) string {
+	before := new(strings.Builder)
+	after := new(strings.Builder)
+	target := after
+	for e := segments.Front(); e != nil; e = e.Next() {
+		if e.Value == 'S' {
+			target = before
+		}
+		target.WriteRune(e.Value)
+	}
+	before.WriteString(after.String())
+	return before.String()
 }
